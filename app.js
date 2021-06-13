d3183dff4fe760317156ef255a69570fa76a6392
hazelisles <zhccj19@gmail.com>
2021-06-14 03:07:58 +0800

    feat: add passport and express-session

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-
+const session = require('express-session')
 
 const app = express()
 const PORT = 3000
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'NooneFmyfamily',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(methodOverride('_method'))
+usePassport(app)
 app.use(routes)
 
 app.listen(PORT, () => {
---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) {
+          return done(null, false, req.flash('error_message', '此 email 尚未註冊！'), req.flash('loginemail', email))
+        }
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, req.flash('error_message', 'Email 或 Password 不正確！'), req.flash('loginemail', email))
+          }
+          return done(null, user)
+        })
+      })
+      .catch(err => done(err, false))
+  }))
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findByPk(id)
+      .then((user) => {
+        user = user.toJSON()
+        done(null, user)
+      })
+      .catch(err => done(err, null))
+  })
+}
