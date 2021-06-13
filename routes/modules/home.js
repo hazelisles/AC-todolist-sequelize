5b5a87d6dd32f84f6632b175b94c51c3712b46e1
hazelisles <zhccj19@gmail.com>
2021-06-14 02:27:16 +0800

    feat: add index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
 
 router.get('/', (req, res) => {
-  res.send('hello world!')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 module.exports = router
