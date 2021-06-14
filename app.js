56067c1edc214865eeeb976e1df63a4bfeb7cbae
hazelisles <zhccj19@gmail.com>
2021-06-14 21:43:59 +0800

    feat: add dotenv and passport-facebook

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 
@@ -13,7 +15,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
-  secret: 'NooneFmyfamily',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
