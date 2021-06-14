359e9d4d5763e55362d4ee86b4d650350800a5b4
hazelisles <zhccj19@gmail.com>
2021-06-14 15:27:39 +0800

    feat: add flash message and express-validator

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 const PORT = 3000
@@ -18,6 +19,16 @@ app.use(session({
 }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.loginemail = req.flash('loginemail')
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error_msg = req.flash('error_message')
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,12 +4,26 @@ const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const db = require('../../models')
 const User = db.User
+const { check, validationResult } = require('express-validator')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', passport.authenticate('local', {
+router.post('/login', [check('email').notEmpty().withMessage('請輸入Email！'), check('password').notEmpty().withMessage('請輸入密碼！')], (req, res, next) => {
+  const error = validationResult(req)
+  if (!error.isEmpty()) {
+    const loginemail = req.body.email
+    const errors = error.array()
+    return res.render('login', {
+      errors,
+      loginemail,
+    })
+  }
+  if (error.isEmpty()) {
+    next()
+  }
+}, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
@@ -55,6 +69,7 @@ router.post('/register', (req, res) => {
 
 router.get('logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
