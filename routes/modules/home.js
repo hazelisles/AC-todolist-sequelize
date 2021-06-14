a71307156dec30b251227546d5a4b13600d44dab
hazelisles <zhccj19@gmail.com>
2021-06-14 16:36:46 +0800

    feat: add create function and new page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,14 +4,26 @@ const db = require('../../models')
 const Todo = db.Todo
 
 router.get('/', (req, res) => {
-  const userId = req.user.id
+  const UserId = req.user.id
   return Todo.findAll({
-    where: { userId },
+    where: { UserId },
     raw: true,
     nest: true
   })
     .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => { return res.status(422).json(error) })
+})
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const { name } = req.body
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
